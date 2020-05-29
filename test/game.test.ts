6c19cda42b4d2f223a34a37dbad76d1a970e9557
Varesh Tapadia <varesh.tapadia@verifone.com>
2020-05-29 16:57:10 +0200

    Added sample test and .travis.yml file

---
+++ test/game.test.ts
@@ -0,0 +1,22 @@
+import * as Lab from '@hapi/lab';
+import { expect } from '@hapi/code';
+// import { afterEach, beforeEach, describe, it } = exports.lab = Lab.script();
+
+const lab = Lab.script();
+const { describe, it, beforeEach } = lab;
+export { lab };
+
+import {Board} from "../src/game";
+
+
+describe("basic game", () => {
+  let game:Board;
+  beforeEach(() => {
+    game = new Board(1);
+    game.print();
+  });
+
+  it("test", () => {
+    console.log("all good");
+  })
+})
